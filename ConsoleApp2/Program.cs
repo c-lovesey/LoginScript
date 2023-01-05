faa900182f4ee1bd6f789fccafe1ea62f75783d4
c-lovesey <85715925+c-lovesey@users.noreply.github.com>
2023-01-05 09:30:32 +0000

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        bool login = false;
+        while (login == false)
+        {
+
+
+            // Prompt the user for their username
+            Console.WriteLine("Please enter your username:");
+            string? username = Console.ReadLine();
+
+            // Prompt the user for their password
+            Console.WriteLine("Please enter your password:");
+            string? password = Console.ReadLine();
+
+            // Check if the entered credentials are valid
+            if (username == "admin" && password == "password")
+            {
+                Console.WriteLine("Login successful!");
+                login = true;
+            }
+            else
+            {
+                Console.WriteLine("Invalid username or password.");
+            }
+        }
+    }
+}
